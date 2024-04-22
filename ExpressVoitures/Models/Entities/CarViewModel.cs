23788cdf9571c20adf0de4f43c290c583ac7fbf9
EveCrystali <evequeenrevenge@gmail.com>
2024-04-22 13:33:17 +0200

    feat(Create): improve UI for Car's creation
    - several UI improvement (architecture) in CSS and HTML to align with figma prototype
    - ensure dropdown appearance with `-webkit-appearance`
    
    Resolve issue: Create the structure of the HTML + CSS/JS : Register New Car #12

--- ExpressVoitures/Models/Entities/CarViewModel.cs
+++ ExpressVoitures/Models/Entities/CarViewModel.cs
@@ -8,7 +8,7 @@ namespace ExpressVoitures.Models.Entities
     {
         public int Id { get; set; }
 
-        [DisplayName("Marque*")]
+        [DisplayName("Marque* :")]
         public int CarBrandId { get; set; }
 
         
@@ -17,20 +17,20 @@ namespace ExpressVoitures.Models.Entities
         
         public string? CarBrandName { get; set; }
 
-        [DisplayName("Modèle*")]
+        [DisplayName("Modèle* :")]
         public int CarModelId { get; set; }
 
         public CarModel? CarModel { get; set; }
 
         public string? CarModelName { get; set; }
 
-        [DisplayName("Finition*")]
+        [DisplayName("Finition* :")]
         public int CarTrimId { get; set; }
         public CarTrim? CarTrim { get; set; }
 
         public string? CarTrimName { get; set; }
 
-        [DisplayName("Motorisation")]
+        [DisplayName("Motorisation :")]
         public int CarMotorId { get; set; }
 
         public CarMotor? CarMotor { get; set; }
@@ -39,43 +39,43 @@ namespace ExpressVoitures.Models.Entities
 
         public List<CarRepair> CarRepairs { get; set; } = [];
 
-        [DisplayName("Année*")]
+        [DisplayName("Année* :")]
         [Required(ErrorMessage = "L'année du véhicule est obligatoire.")]
         [Range(1990, 2024, ErrorMessage = "La valeur doit être comprise entre 1990 et 2024.")]
         public int? Year { get; set; }
 
-        [DisplayName("Kilométrage")]
+        [DisplayName("Kilométrage :")]
         public int? Mileage { get; set; }
 
-        [DisplayName("Prix d'achat*")]
+        [DisplayName("Prix d'achat* :")]
         [Required(ErrorMessage = "Le prix d'achat du véhicule est obligatoire.")]
         [Range(0.01, Double.MaxValue, ErrorMessage = "La valeur doit être positive.")]
         public decimal? PurchasePrice { get; set; }
 
-        [DisplayName("Prix de vente")]
+        [DisplayName("Prix de vente :")]
         [Range(0.01, Double.MaxValue, ErrorMessage = "La valeur doit être positive.")]
         public decimal? SellingPrice { get; set; }
 
-        [DisplayName("Disponibile*")]
+        [DisplayName("Disponibile* :")]
         [Required(ErrorMessage = "La disponibilité du vehicule est obligatoire.")]
         public bool IsAvailable { get; set; }
 
-        [DisplayName("Date d'achat")]
+        [DisplayName("Date d'achat :")]
         public DateOnly? PurchaseDate { get; set; }
 
-        [DisplayName("Date de disponibilité à la vente")]
+        [DisplayName("Disponibilité à la vente :")]
         public DateOnly? DateOfAvailability { get; set; }
 
-        [DisplayName("Date de vente")]
+        [DisplayName("Date de vente :")]
         public DateOnly? SaleDate { get; set; }
 
-        [DisplayName("Description")]
+        [DisplayName("Description :")]
         public string? Description { get; set; }
 
-        [DisplayName("Chemin des images")]
+        [DisplayName("Chemin des images :")]
         public List<String>? ImagePaths { get; set; }
 
-        [DisplayName("Images")]
+        [DisplayName("Images :")]
         public List<IFormFile>? Images { get; set; }
     }
 }
